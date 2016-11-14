86dd4fbd38d907314bccd1fd31b4a13ff8733641
Dave Z <dave@flysorter.com>
2016-11-14 06:34:16 +0000

    Using callback thread to handle image saving.

--- CameraTest.cpp
+++ CameraTest.cpp
@@ -1,10 +1,35 @@
 #include <pylon/PylonIncludes.h>
 
+#include "Samples/C++/include/ConfigurationEventPrinter.h"
+#include "Samples/C++/include/ImageEventPrinter.h"
+
 // Namespace for using pylon objects.
 using namespace Pylon;
 
 // Namespace for using cout.
 using namespace std;
+
+//Example of an image event handler.
+class CImageSaver : public CImageEventHandler
+{
+public:
+    virtual void OnImageGrabbed( CInstantCamera& camera, const CGrabResultPtr& ptrGrabResult)
+    {
+        cout << "Got result." << endl;
+        if (ptrGrabResult->GrabSucceeded())
+        {
+            CPylonImage image;
+            image.AttachGrabResultBuffer( ptrGrabResult);
+            cout << "Got image." << endl;
+            image.Save(ImageFileFormat_Png, "images/sample.png");
+            cout << "Saved." << endl;
+            image.Release();
+            cout << "Released." << endl;
+        }
+
+
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -12,45 +37,40 @@ int main(int argc, char* argv[])
     int exitCode = 0;
     CGrabResultPtr ptrGrabResult;
 
-
     // Before using any pylon methods, the pylon runtime must be initialized.
     PylonInitialize();
 
     try
     {
         // Create an instant camera object with the camera device found first.
+        // As soon as this object goes out of scope, the thread is killed and
+        // no callbacks occur.
         CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice());
 
         // Print the model name of the camera.
         cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
 
         camera.RegisterConfiguration( new CSoftwareTriggerConfiguration, RegistrationMode_ReplaceAll, Cleanup_Delete);
+        camera.RegisterConfiguration( new CConfigurationEventPrinter, RegistrationMode_Append, Cleanup_Delete);
+        camera.RegisterImageEventHandler( new CImageEventPrinter, RegistrationMode_Append, Cleanup_Delete);
+        camera.RegisterImageEventHandler( new CImageSaver, RegistrationMode_Append, Cleanup_Delete);
         camera.Open();
 
         cout << "Opened." << endl;
-
-        camera.StartGrabbing(1);
-
-        cout << "Started grabbing." << endl;
-
-        while(camera.IsGrabbing()) {
-            camera.WaitForFrameTriggerReady( 100, TimeoutHandling_ThrowException);
-            camera.ExecuteSoftwareTrigger();
-            cout << "Triggered." << endl;
-            camera.RetrieveResult(500, ptrGrabResult, TimeoutHandling_ThrowException);
-            if (ptrGrabResult->GrabSucceeded())
+        if (camera.CanWaitForFrameTriggerReady()) {
+            camera.StartGrabbing( GrabStrategy_OneByOne, GrabLoop_ProvidedByInstantCamera);
+            cout << "Started grabbing." << endl;
+            if ( camera.WaitForFrameTriggerReady( 100, TimeoutHandling_ThrowException))
             {
-                cout << "Got result." << endl;
-                CPylonImage image;
-                image.AttachGrabResultBuffer( ptrGrabResult);
-                cout << "Got image." << endl;
-                image.Save(ImageFileFormat_Png, "images/sample.png");
-                cout << "Saved." << endl;
-                image.Release();
-                cout << "Released." << endl;
+                camera.ExecuteSoftwareTrigger();
+                cout << "Triggered." << endl;
             }
-
+        } else {
+            cout << "Can't wait for frame trigger on this camera?" << endl;
         }
+
+        cout << "Sleeping." << endl;
+        WaitObject::Sleep( 3*1000);
 
     }
     catch (const GenericException &e)
